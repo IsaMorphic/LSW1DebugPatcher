140997c0a8e3d1b232d602716a03ae24ff2a8795
Yodadude2003 <yodadude2003@gmail.com>
2021-03-30 13:54:37 -0500

    Add project files.

---
+++ LSW1DebugPatcher/Patch.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace LSW1DebugPatcher
+{
+    public class Patch
+    {
+        private const string PATTERN = "([0|1|2|3|4|5|6|7|8|9|A|B|C|D|E|F]{8}) : ([0|1|2|3|4|5|6|7|8|9|A|B|C|D|E|F]{2})\\|([0|1|2|3|4|5|6|7|8|9|A|B|C|D|E|F]{2})";
+
+        public (long addr, byte apply, byte undo)[] Patches { get; }
+
+        private Patch((long, byte, byte)[] patches)
+        {
+            Patches = patches;
+        }
+
+        public void Use(string path, bool apply = true)
+        {
+            using (var file = File.OpenWrite(path))
+            {
+                foreach (var patch in Patches)
+                {
+                    file.Seek(patch.addr, SeekOrigin.Begin);
+                    file.WriteByte(apply ? patch.apply : patch.undo);
+                }
+            }
+        }
+
+        public static Patch ParseFromFile(string path)
+        {
+            var lineNum = 0;
+
+            var patches = new List<(long, byte, byte)>();
+            using (var file = File.OpenText(path))
+            {
+                while (!file.EndOfStream)
+                {
+                    var line = file.ReadLine();
+                    var matches = Regex.Match(line, PATTERN);
+
+                    lineNum++;
+
+                    if (matches.Success &&
+                        long.TryParse(matches.Groups[1].Value, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out long addr) &&
+                        byte.TryParse(matches.Groups[2].Value, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out byte apply) &&
+                        byte.TryParse(matches.Groups[3].Value, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out byte undo))
+                    {
+                        patches.Add((addr, apply, undo));
+                    }
+                    else
+                    {
+                        throw new FormatException($"Line #{lineNum} of {path} is invalid! Check program documentation for more info.");
+                    }
+                }
+            }
+
+            return new Patch(patches.ToArray());
+        }
+    }
+}
---
+++ LSW1DebugPatcher/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace LSW1DebugPatcher
+{
+    class Program
+    {
+        const string EXE_NAME = "LegoStarwars.exe";
+        const string DEFAULT_PATCH = ".\\en_US.patch";
+
+        static void Main(string[] args)
+        {
+            string descPath;
+            if (args.Length < 1)
+                descPath = DEFAULT_PATCH;
+            else
+                descPath = args[0];
+
+            Console.WriteLine("Creating backup of game exe...");
+            File.Copy($"..\\{EXE_NAME}", $".\\{EXE_NAME}.backup", true);
+
+            Console.WriteLine($"Parsing patch file: {descPath}");
+            Patch patch;
+            try
+            {
+                patch = Patch.ParseFromFile(descPath);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            bool? apply = null;
+            Console.WriteLine("Would you like to enable or disable the patch? (E/D)");
+            do
+            {
+                var resp = Console.ReadLine().ToUpperInvariant();
+
+                if (resp == "E")
+                    apply = true;
+                else if (resp == "D")
+                    apply = false;
+
+                if (!apply.HasValue)
+                    Console.WriteLine("Please enter 'E' if you would like to enable the patch, or 'D' if you would like to disable it.");
+            }
+            while (!apply.HasValue);
+
+            try
+            {
+                patch.Use($"..\\{EXE_NAME}", apply.Value);
+            }
+            catch
+            {
+                Console.WriteLine("Patching failed!!! Restoring backup exe...");
+                File.Copy($".\\{EXE_NAME}.backup", $"..\\{EXE_NAME}", true);
+            }
+
+            Console.WriteLine("All operations have completed successfully.");
+        }
+    }
+}
